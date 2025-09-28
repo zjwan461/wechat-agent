f9b5d22d94cf20cbaae3e542052845389ed69bcc
jerry <826935261@qq.com>
2025-09-28 09:50:26 +0800

    update

--- wechat_agent/conf.py
+++ wechat_agent/conf.py
@@ -1,11 +1,13 @@
+import os
+
 token_header = "Authorization"
 token_prefix = "Bearer "
 token_white_list = [r'^/ui/.*', "/api/auth/login", "/api/auth/register", "/api/base/sys-info",
                     "/api/base/git-repo", "/api/auth/yzm", "/api/auth/refresh-token"]
 gitee_url = "https://gitee.com/zjwan461/wechat-agent"
 github_url = "https://github.com/zjwan461/wechat-agent"
-server_port = 8080
-server_host = "0.0.0.0"
+server_port = os.getenv('wa_port', 8080)
+server_host = os.getenv('wa_host', "0.0.0.0")
 db_url = "sqlite:///{0}/wechat-agent/db/wxa.db"
 sys_info_id = 1
 SECRET_KEY = "your-secret-key-keep-it-safe"
